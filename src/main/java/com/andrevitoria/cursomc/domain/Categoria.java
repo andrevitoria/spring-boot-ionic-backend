d2d6a81668ee52e4fefaf833c0ea34d154e6a3c1
Andre Vitoria <andrevitoria@gmail.com>
2018-08-20 21:28:38 -0300

    adicionado classe manyToMany

--- src/main/java/com/andrevitoria/cursomc/domain/Categoria.java
+++ src/main/java/com/andrevitoria/cursomc/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.andrevitoria.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 @Entity
 public class Categoria implements Serializable {
 	/**
@@ -15,7 +18,16 @@ public class Categoria implements Serializable {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
-	private String nome;
+	private String nome;	
+	@ManyToMany(mappedBy="categorias")	
+	private List<Produto> produtos = new ArrayList<>();
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
 	public Categoria() {}
 	public Integer getId() {
 		return id;
