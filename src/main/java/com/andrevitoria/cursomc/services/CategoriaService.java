f91c54b9696dd9ca72080ff10b7b0a270c2a3c0a
Andre Vitoria <andrevitoria@gmail.com>
2018-09-02 17:02:32 -0300

    inserido o método findAll

--- src/main/java/com/andrevitoria/cursomc/services/CategoriaService.java
+++ src/main/java/com/andrevitoria/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.andrevitoria.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,5 +37,8 @@ public class CategoriaService {
 			throw new DataIntegrityException("não é possível excluir uma categoria que contém produtos!");
 			
 		}
+	}
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 }
