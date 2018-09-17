ed7fcddcce9da18aac321a3179ae2fcbf7f55de2
Andre Vitoria <andrevitoria@gmail.com>
2018-09-17 19:14:55 -0300

    corrigindo atualização de cliente

--- src/main/java/com/andrevitoria/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/andrevitoria/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.andrevitoria.cursomc.domain.Cliente;
+import com.andrevitoria.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private Integer id;
