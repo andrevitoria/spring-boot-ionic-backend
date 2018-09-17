5aa799f228fe6cc79c54b58d0f0f336ea224712e
Andre Vitoria <andrevitoria@gmail.com>
2018-09-17 18:49:43 -0300

    inclusão de validação chave candidata email

--- src/main/java/com/andrevitoria/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/andrevitoria/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,30 +6,43 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.andrevitoria.cursomc.domain.Cliente;
 import com.andrevitoria.cursomc.domain.enums.TipoCliente;
 import com.andrevitoria.cursomc.dto.ClienteNewDTO;
+import com.andrevitoria.cursomc.repositories.ClienteRepository;
 import com.andrevitoria.cursomc.resources.exception.FieldMessage;
 import com.andrevitoria.cursomc.services.validation.utils.BR;
-public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
- @Override
- public void initialize(ClienteInsert ann) {
- }
- @Override
- public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
- List<FieldMessage> list = new ArrayList<>();
 
- // inclua os testes aqui, inserindo erros na lista
- if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())){
-	 list.add(new FieldMessage("cpfOuCnpj","CPF inválido"));
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Autowired
+	private ClienteRepository repo;
+
+	@Override
+	public void initialize(ClienteInsert ann) {
 	}
-	if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())){
-	 list.add(new FieldMessage("cpfOuCnpj","CNPJ inválido"));
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+
+		// inclua os testes aqui, inserindo erros na lista
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if ( aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
 	}
- for (FieldMessage e : list) {
- context.disableDefaultConstraintViolation();
- context.buildConstraintViolationWithTemplate(e.getMessage())
- .addPropertyNode(e.getFieldName()).addConstraintViolation();
- }
- return list.isEmpty();
- }
 }
