5f5f97df739f48fc8b6530d0ea11d059ea945b1d
Andre Vitoria <andrevitoria@gmail.com>
2018-10-21 19:33:31 -0300

    enviando email HTML

--- src/main/java/com/andrevitoria/cursomc/services/EmailService.java
+++ src/main/java/com/andrevitoria/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.andrevitoria.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.andrevitoria.cursomc.domain.Pedido;
@@ -7,5 +9,6 @@ import com.andrevitoria.cursomc.domain.Pedido;
 public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	void sendMail(SimpleMailMessage msg);
-	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	void sendHtmlEmail(MimeMessage msg);
 }
